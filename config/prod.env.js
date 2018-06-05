853c616fe221e6b93dcaee75fe5a21c00ab5baf1
王玲波 <454949876@qq.com>
2018-06-05 17:15:53 +0800

    ### *柏林* 数据中心打包

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,12 +2,12 @@
  * @Author: FT.FE.Bolin
  * @Date: 2018-04-11 16:31:37
  * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-06-04 14:52:23
+ * @Last Modified time: 2018-06-05 17:09:03
  */
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
   NPM_CONFIG_REPORT: '"true"',
   /* 线上API地址 */
-  BASE_API: '"https://datacenter.mdguanjia.com"'
+  BASE_API: '"https://datacenter.mdguanjia.com/datacenter/"'
 }
--- config/sit.env.js
+++ config/sit.env.js
@@ -2,7 +2,7 @@
  * @Author: FT.FE.Bolin
  * @Date: 2018-04-11 16:31:45
  * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-06-04 14:52:17
+ * @Last Modified time: 2018-06-05 17:09:10
  */
 
 module.exports = {
@@ -10,5 +10,5 @@ module.exports = {
   ENV_CONFIG: '"sit"',
   NPM_CONFIG_REPORT: '"true"',
   /* 测试环境API地址 */
-  BASE_API: '"https://datacenter.mdguanjia.com"'
+  BASE_API: '"https://datacenter.mdguanjia.com/datacenter/"'
 }
