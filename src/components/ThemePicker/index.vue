7749e3d0dbc7cce56272cb85c8b2ea607f903cd4
王玲波 <454949876@qq.com>
2018-06-07 16:23:23 +0800

    ### *柏林*  本地环境自定义可以换肤  为何服务器环境不行呢

--- src/components/ThemePicker/index.vue
+++ src/components/ThemePicker/index.vue
@@ -2,7 +2,7 @@
  * @Author: FT.FE.Bolin
  * @Date: 2018-04-11 17:04:21
  * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-06-07 15:16:06
+ * @Last Modified time: 2018-06-07 16:21:36
  */
 
 <template>
@@ -63,9 +63,12 @@ export default {
           const text = style.innerText
           return new RegExp(oldVal, 'i').test(text) && !/Chalk Variables/.test(text)
         })
+      console.log(styles)
       styles.forEach(style => {
-        const { innerText } = style
-        if (typeof innerText !== 'string') return
+        const innerText = style.innerText
+        if (typeof innerText !== 'string') {
+          return
+        }
         style.innerText = this.updateStyle(innerText, originalCluster, themeCluster)
       })
       localStorage.setItem('ORIGINAL_THEME', val)
